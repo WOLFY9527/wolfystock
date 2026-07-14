e5244d51ed0e4491257f52d7115383a80ad05ae6
WOLFY9527 <leeyiphang@gmail.com>
2026-07-14 11:11:40 +0800

    chore(repo): remove orphan assets and duplicate tests

--- tests/api/test_market_rates.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compatibility test entrypoint for the rates-focused market macro checks."""
-
-from tests.api.test_market_macro_cards import *  # noqa: F401,F403
--- tests/api/test_system_config.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Security regression tests for system config API masking."""
-
-from tests.api.test_settings_data_source_validation import SettingsDataSourceValidationApiTestCase
-from tests.test_system_config_api import SystemConfigApiTestCase
-
-
-__all__ = ["SystemConfigApiTestCase", "SettingsDataSourceValidationApiTestCase"]
